97fc562631273899408b5118a74dc84803325bac
DucAnh <ngducanh1223@gmail.com>
2020-06-23 18:23:06 +0700

    Get all success conversations

--- README.md
+++ README.md
@@ -1,2 +1,43 @@
 # everything_analysis
-analyze everything I found
+
+###API
+```TOKEN=$(curl -s https://babeshop.ftech.ai/api/auth -d '{"username": "me", "password": "w4J6OObi996nDGcQ4mlYNK4F"}' | jq -r .access_token)```
+
+```curl -H "Authorization: Bearer $TOKEN" -s https://babeshop.ftech.ai/api/conversations > conv.json```
+
+```curl -H "Authorization: Bearer $TOKEN" -s https://babeshop.ftech.ai/api/conversations/3249860898460524 | less```
+
+```"sender_id": 3249860898460524```
+##Purpose 
+Develop core metrics to track performance of salesbot. 
+##Approach/ Propositions 
+Proposed metric: 
+(Level 1) Success Rate: is defined as below 
+
+Number of successful conversationsAll conversations
+
+
+Successful conversations: successful conversations are marked by thanks or order/ shipping requests at the end of the conversations. 
+
+To count number of successful conversations, we count: 
+how many chatlogs (conversations) end up with thanks 
+how many chatlogs (conversations) end up with order request 
+and then add them up together 
+
+[Describe how you count "thanks" chatlogs]
+
+..............
+
+
+
+[Describe how you count "order/ shipping" chatlogs]
+
+...............
+##Data Description
+[You may have to briefly describe the data (chatlog) you analyze . How many chatlog in total, some descriptive statistics might be interesting such as the average number of turns of each conversations, since when bot goes live, etc.]
+##Results 
+
+We report success rate over days/ weeks/ months since bot went live, and report results across versions. 
+[Insert results, provide graphs & charts, ...] 
+
+##Discussions
---
+++ do_analyze.py
@@ -0,0 +1,145 @@
+import pandas as pd
+import numpy as np
+import json
+import os
+import datetime
+import pickle
+from csv import DictWriter
+from ast import literal_eval
+import logging
+
+logging.root.setLevel(logging.NOTSET)
+logging.basicConfig(
+    level=logging.NOTSET,
+    format='%(asctime)s %(module)s - %(funcName)s: %(message)s',
+    datefmt='%Y-%m-%d %H:%M:%S'
+)
+logger = logging.getLogger(__name__)
+
+def get_all_conv():
+    with open('chatlog_data/all_conv.pkl', 'rb') as f:
+        data = pickle.load(f)
+    return data
+
+
+def get_all_conv_detail():
+    all_conv_detail_df = pd.read_csv("chatlog_data/all_conv_detail.csv",
+                                     names=["sender_id", "slots", "latest_message", "latest_event_time",
+                                            "followup_action", "paused", "events", "latest_input_channel",
+                                            "active_form", "latest_action_name"
+                                            ])
+    return all_conv_detail_df
+
+
+def append_dict_as_row(file_name, dict_of_elem, field_names):
+    with open(file_name, 'a+', newline='') as write_obj:
+        dict_writer = DictWriter(write_obj, fieldnames=field_names)
+        dict_writer.writerow(dict_of_elem)
+
+
+def export_conversations():
+    conversation_api = "curl -H \"Authorization: Bearer $TOKEN\" -s https://babeshop.ftech.ai/api/conversations"
+    token = "TOKEN=$(curl -s https://babeshop.ftech.ai/api/auth -d '{\"username\": \"me\", \"password\": \"w4J6OObi996nDGcQ4mlYNK4F\"}' | jq -r .access_token)"
+    all_conversations = json.loads(os.popen(token + " && " + conversation_api).read())
+    with open('chatlog_data/all_conv.pkl', 'wb') as f:
+        pickle.dump(all_conversations, f)
+
+
+def export_conversation_detail():
+    file_name = "chatlog_data/all_conv_detail.csv"
+    all_conv = get_all_conv()
+    all_sender_id = [x["sender_id"] for x in all_conv]
+    for sender_id in all_sender_id:
+        conversation_detail_api = "curl -H \"Authorization: Bearer $TOKEN\" -s https://babeshop.ftech.ai/api/conversations/{}"
+        token = "TOKEN=$(curl -s https://babeshop.ftech.ai/api/auth -d '{\"username\": \"me\", \"password\": \"w4J6OObi996nDGcQ4mlYNK4F\"}' | jq -r .access_token)"
+        if os.popen(token + " && " + conversation_detail_api.format(sender_id)).read() is not None:
+            try:
+                conversation_detail = json.loads(
+                    os.popen(token + " && " + conversation_detail_api.format(sender_id)).read())
+                field_names = list(conversation_detail.keys())
+                append_dict_as_row(file_name, conversation_detail, field_names)
+                logger.info("Add row to file")
+            except Exception as ex:
+                logger.error(ex)
+
+        a = 0
+
+
+def extract_info_from_conversation(conversation_detail, info: dict):
+    """
+
+    :param conversation_detail:
+    :param info:
+    :return: info dictionary
+    """
+    begin_time = datetime.datetime.utcfromtimestamp(int(conversation_detail["events"][0]["timestamp"]))
+    end_time = datetime.datetime.utcfromtimestamp(int(conversation_detail["events"][-1]["timestamp"]))
+
+    user_messages_in_conversation = [x for x in conversation_detail["events"] if x["event"] == "user"]
+    if user_messages_in_conversation[-1]["parse_data"]['intent']["name"] == "thank":
+        info["thank"].append(1)
+    else:
+        info["thank"].append(0)
+    info["sender_id"].append("a")
+    info["begin"].append(begin_time)
+    info["end"].append(end_time)
+    return info
+
+
+def main():
+    all_conv_detail = get_all_conv_detail()
+    necessary_info = {"sender_id": [], "begin": [], "end": [], "thank": [], "handover": [], "message": []}
+
+    for conversation in all_conv_detail.itertuples():
+        events = literal_eval(conversation.events)
+        begin_time = datetime.datetime.utcfromtimestamp(int(events[0]["timestamp"]))
+        end_time = datetime.datetime.utcfromtimestamp(int(events[-1]["timestamp"]))
+        user_messages_in_conversation = [x for x in events if x["event"] == "user"]
+        if len(user_messages_in_conversation) > 0:
+            user_intent = user_messages_in_conversation[-1]["parse_data"]['intent']["name"]
+            if user_intent == "thank" and len(user_messages_in_conversation) > 1:
+                necessary_info["thank"].append(1)
+                necessary_info["message"].append(user_messages_in_conversation[-1]["parse_data"]["text"])
+            else:
+                necessary_info["thank"].append(0)
+
+
+            if user_intent == "handover_to_inbox" and len(user_messages_in_conversation) > 1:
+                necessary_info["handover"].append(1)
+                necessary_info["message"].append(user_messages_in_conversation[-1]["parse_data"]["text"])
+            else:
+                necessary_info["handover"].append(0)
+
+            if user_intent not in ["handover_to_inbox", "thank"] or len(user_messages_in_conversation) <= 1:
+                necessary_info["message"].append("")
+
+            necessary_info["sender_id"].append(conversation.sender_id)
+            necessary_info["begin"].append(begin_time)
+            necessary_info["end"].append(end_time)
+
+    necessary_info_df = pd.DataFrame.from_dict(necessary_info)
+    success_conv = necessary_info_df[necessary_info_df["thank"] == 1]
+    key_word = ["ship", "gửi", "lấy", "địa chỉ", "giao hàng","đ/c", "sẵn hàng", "hàng sẵn", "thanh toán", "tổng"]
+    all_handover_df = necessary_info_df[necessary_info_df["handover"] == 1]
+    all_handover_df_sub = all_handover_df.copy()
+    for index, item in all_handover_df.iterrows():
+        a = 0
+        for word_index, word in enumerate(key_word):
+            if word in item["message"].lower():
+                break
+            else:
+                if word_index == len(key_word) - 1:
+                    try:
+                        all_handover_df_sub = all_handover_df_sub.drop(index)
+                    except:
+                        a = 0
+
+    a = 0
+
+
+# export_conversations()
+# export_conversation_detail()
+main()
+
+# chatlog from 18/12/2019 to 19/6/2020
+# ["ship", "gửi", "lấy", "địa chỉ", "giao hàng","đ/c", "sẵn hàng", "hàng sẵn", "thanh toán", "tổng"]
