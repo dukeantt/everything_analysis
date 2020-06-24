49445f8ea3f0d1c7b7a40a666150851d54f186f7
DucAnh <ngducanh1223@gmail.com>
2020-06-24 10:39:32 +0700

    Finish filter thank,order convo; remove trash in all convo

--- do_analyze.py
+++ do_analyze.py
@@ -16,6 +16,7 @@ logging.basicConfig(
 )
 logger = logging.getLogger(__name__)
 
+
 def get_all_conv():
     with open('chatlog_data/all_conv.pkl', 'rb') as f:
         data = pickle.load(f)
@@ -23,6 +24,9 @@ def get_all_conv():
 
 
 def get_all_conv_detail():
+    """
+    Get conversation detail from file
+    """
     all_conv_detail_df = pd.read_csv("chatlog_data/all_conv_detail.csv",
                                      names=["sender_id", "slots", "latest_message", "latest_event_time",
                                             "followup_action", "paused", "events", "latest_input_channel",
@@ -38,6 +42,9 @@ def append_dict_as_row(file_name, dict_of_elem, field_names):
 
 
 def export_conversations():
+    """
+    Export all conversation to file so that we dont have to crawl everytime
+    """
     conversation_api = "curl -H \"Authorization: Bearer $TOKEN\" -s https://babeshop.ftech.ai/api/conversations"
     token = "TOKEN=$(curl -s https://babeshop.ftech.ai/api/auth -d '{\"username\": \"me\", \"password\": \"w4J6OObi996nDGcQ4mlYNK4F\"}' | jq -r .access_token)"
     all_conversations = json.loads(os.popen(token + " && " + conversation_api).read())
@@ -46,6 +53,9 @@ def export_conversations():
 
 
 def export_conversation_detail():
+    """
+    Export all conversation detail to file so that we dont have to crawl everytime
+    """
     file_name = "chatlog_data/all_conv_detail.csv"
     all_conv = get_all_conv()
     all_sender_id = [x["sender_id"] for x in all_conv]
@@ -62,52 +72,60 @@ def export_conversation_detail():
             except Exception as ex:
                 logger.error(ex)
 
-        a = 0
+
+def count_message_on_day(level: int, user_messages_in_conversation):
+    timestamp_int = int(user_messages_in_conversation[-level]["timestamp"])
+    timestamp = datetime.datetime.utcfromtimestamp(timestamp_int).strftime('%Y-%m-%d')
+    message_counter = 0
+    for i in range(0, len(user_messages_in_conversation) - level):
+        timestamp_int_2 = int(user_messages_in_conversation[i]["timestamp"])
+        timestamp_2 = datetime.datetime.utcfromtimestamp(timestamp_int_2).strftime('%Y-%m-%d')
+        if timestamp_2 == timestamp:
+            message_counter += 1
+    return message_counter
 
 
-def extract_info_from_conversation(conversation_detail, info: dict):
+def get_success_conv(all_conv_detail, level: int):
     """
+    level 1: customer message of last turn
 
-    :param conversation_detail:
-    :param info:
-    :return: info dictionary
+    :param all_conv_detail:
+    :param level:
+    :return: dataframe of success conversation by each level
+
     """
-    begin_time = datetime.datetime.utcfromtimestamp(int(conversation_detail["events"][0]["timestamp"]))
-    end_time = datetime.datetime.utcfromtimestamp(int(conversation_detail["events"][-1]["timestamp"]))
-
-    user_messages_in_conversation = [x for x in conversation_detail["events"] if x["event"] == "user"]
-    if user_messages_in_conversation[-1]["parse_data"]['intent']["name"] == "thank":
-        info["thank"].append(1)
-    else:
-        info["thank"].append(0)
-    info["sender_id"].append("a")
-    info["begin"].append(begin_time)
-    info["end"].append(end_time)
-    return info
-
-
-def get_success_conv(all_conv_detail, level:int):
-    necessary_info = {"sender_id": [], "begin": [], "end": [], "thank": [], "handover": [], "message": []}
+    necessary_info = {"sender_id": [], "begin": [], "end": [], "thank": [], "handover": [], "message": [], "level": []}
+    conversation_id_to_remove = []
     for conversation in all_conv_detail.itertuples():
         events = literal_eval(conversation.events)
         begin_time = datetime.datetime.utcfromtimestamp(int(events[0]["timestamp"]))
         end_time = datetime.datetime.utcfromtimestamp(int(events[-1]["timestamp"]))
         user_messages_in_conversation = [x for x in events if x["event"] == "user"]
-        # if len(user_messages_in_conversation) > 0:
         if len(user_messages_in_conversation) > (level - 1):
             user_intent = None
             if "name" in user_messages_in_conversation[-level]["parse_data"]['intent']:
                 user_intent = user_messages_in_conversation[-level]["parse_data"]['intent']["name"]
+            message_count_on_day = count_message_on_day(level, user_messages_in_conversation)
+
             if user_intent == "thank" and len(user_messages_in_conversation) > 1:
-                necessary_info["thank"].append(1)
-                necessary_info["message"].append(user_messages_in_conversation[-level]["parse_data"]["text"])
+                if message_count_on_day < 2:
+                    necessary_info["thank"].append(0)
+                    necessary_info["message"].append(user_messages_in_conversation[-level]["parse_data"]["text"])
+                    conversation_id_to_remove.append(conversation.sender_id)
+                else:
+                    necessary_info["thank"].append(1)
+                    necessary_info["message"].append(user_messages_in_conversation[-level]["parse_data"]["text"])
             else:
                 necessary_info["thank"].append(0)
 
-
             if user_intent == "handover_to_inbox" and len(user_messages_in_conversation) > 1:
-                necessary_info["handover"].append(1)
-                necessary_info["message"].append(user_messages_in_conversation[-level]["parse_data"]["text"])
+                if message_count_on_day < 2:
+                    necessary_info["handover"].append(0)
+                    necessary_info["message"].append(user_messages_in_conversation[-level]["parse_data"]["text"])
+                    conversation_id_to_remove.append(conversation.sender_id)
+                else:
+                    necessary_info["handover"].append(1)
+                    necessary_info["message"].append(user_messages_in_conversation[-level]["parse_data"]["text"])
             else:
                 necessary_info["handover"].append(0)
 
@@ -117,6 +135,7 @@ def get_success_conv(all_conv_detail, level:int):
             necessary_info["sender_id"].append(conversation.sender_id)
             necessary_info["begin"].append(begin_time)
             necessary_info["end"].append(end_time)
+            necessary_info["level"].append(level)
 
     necessary_info_df = pd.DataFrame.from_dict(necessary_info)
     success_conv = necessary_info_df[necessary_info_df["thank"] == 1]
@@ -137,16 +156,29 @@ def get_success_conv(all_conv_detail, level:int):
     for word in filter_word:
         all_handover_df_sub = all_handover_df_sub[~all_handover_df_sub["message"].str.lower().str.contains(word)]
     combine_df = pd.concat([all_handover_df_sub, success_conv])
-    return combine_df
+    return combine_df, conversation_id_to_remove
+
 
 def main():
     all_conv_detail = get_all_conv_detail()
-    result_df1 = get_success_conv(all_conv_detail, level=1)
-    result_df2 = get_success_conv(all_conv_detail, level=2)
-    result_df3 = get_success_conv(all_conv_detail, level=3)
+    result_df1, conversation_id_to_remove_1 = get_success_conv(all_conv_detail, level=1)
+    result_df2, conversation_id_to_remove_2 = get_success_conv(all_conv_detail, level=2)
+    result_df3, conversation_id_to_remove_3 = get_success_conv(all_conv_detail, level=3)
     result_df = pd.concat([result_df3, result_df2, result_df1])
-    result_df = result_df.sort_values(by="begin", ascending=False).drop_duplicates(subset=["sender_id"], keep="first")
-    a = 0
+    result_df = result_df.sort_values(by=["begin", "level"], ascending=False).drop_duplicates(subset=["sender_id"],
+                                                                                              keep="first")
+    conversation_id_to_remove = conversation_id_to_remove_1 + conversation_id_to_remove_2 + conversation_id_to_remove_3
+    conversation_id_to_remove = list(set(conversation_id_to_remove))
+    result_sender_id_list = list(result_df["sender_id"])
+    for item in conversation_id_to_remove:
+        if item in result_sender_id_list:
+            conversation_id_to_remove.remove(item)
+    all_conv_detail_without_trash = all_conv_detail[~all_conv_detail["sender_id"].isin(conversation_id_to_remove)]
+
+    all_conv_detail_without_trash.to_csv("analyze_data/all_conversations_without_trash.csv", index=False)
+    result_df.to_csv("analyze_data/success_conversations.csv", index=False)
+
+
 
 # export_conversations()
 # export_conversation_detail()
