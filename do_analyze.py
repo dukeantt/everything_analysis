58cd65960704e4efec840d531da94b27d7cc9449
DucAnh <ngducanh1223@gmail.com>
2020-06-25 17:53:41 +0700

    Fix count conversations

--- do_analyze.py
+++ do_analyze.py
@@ -74,11 +74,11 @@ def export_conversation_detail():
                 logger.error(ex)
 
 
-def count_message_on_day(level: int, user_messages_in_conversation):
-    timestamp_int = int(user_messages_in_conversation[-level]["timestamp"])
+def count_message_on_day(user_message, user_messages_in_conversation):
+    timestamp_int = int(user_message["timestamp"])
     timestamp = datetime.datetime.utcfromtimestamp(timestamp_int).strftime('%Y-%m-%d')
     message_counter = 0
-    for i in range(0, len(user_messages_in_conversation) - level):
+    for i in range(0, len(user_messages_in_conversation)):
         timestamp_int_2 = int(user_messages_in_conversation[i]["timestamp"])
         timestamp_2 = datetime.datetime.utcfromtimestamp(timestamp_int_2).strftime('%Y-%m-%d')
         if timestamp_2 == timestamp:
@@ -86,71 +86,70 @@ def count_message_on_day(level: int, user_messages_in_conversation):
     return message_counter
 
 
-def get_success_conv(all_conv_detail, level: int):
+def get_success_conv(all_conv_detail):
     """
-    level 1: customer message of last turn
 
     :param all_conv_detail:
-    :param level:
-    :return: dataframe of success conversation by each level
+    :return: dataframe of success conversation
 
     """
     necessary_info = {"sender_id": [], "begin": [], "end": [], "message_timestamp": [], "message_timestamp_month": [],
                       "message_timestamp_date": [], "thank": [], "handover": [],
-                      "message": [], "obj_type": [], "level": []}
+                      "message": [], "obj_type": [],
+                      }
     conversation_id_to_remove = []
     for conversation in all_conv_detail.itertuples():
         events = literal_eval(conversation.events)
         begin_time = datetime.datetime.utcfromtimestamp(int(events[0]["timestamp"]))
         end_time = datetime.datetime.utcfromtimestamp(int(events[-1]["timestamp"]))
         user_messages_in_conversation = [x for x in events if x["event"] == "user"]
-        if len(user_messages_in_conversation) > (level - 1):
-            user_intent = None
-            if "name" in user_messages_in_conversation[-level]["parse_data"]['intent']:
-                user_intent = user_messages_in_conversation[-level]["parse_data"]['intent']["name"]
-            message_count_on_day = count_message_on_day(level, user_messages_in_conversation)
+        if len(user_messages_in_conversation) > 1:
+            for user_message in user_messages_in_conversation:
+                user_intent = None
+                if "name" in user_message["parse_data"]['intent']:
+                    user_intent = user_message["parse_data"]['intent']["name"]
+                message_count_on_day = count_message_on_day(user_message, user_messages_in_conversation)
 
-            if user_intent == "thank" and len(user_messages_in_conversation) > 1:
-                if message_count_on_day < 2:
+                if user_intent == "thank" and len(user_messages_in_conversation) > 1:
+                    if message_count_on_day < 2:
+                        necessary_info["thank"].append(0)
+                        necessary_info["message"].append(user_message["parse_data"]["text"])
+                        conversation_id_to_remove.append(conversation.sender_id)
+                    else:
+                        necessary_info["thank"].append(1)
+                        necessary_info["message"].append(user_message["parse_data"]["text"])
+                else:
                     necessary_info["thank"].append(0)
-                    necessary_info["message"].append(user_messages_in_conversation[-level]["parse_data"]["text"])
-                    conversation_id_to_remove.append(conversation.sender_id)
-                else:
-                    necessary_info["thank"].append(1)
-                    necessary_info["message"].append(user_messages_in_conversation[-level]["parse_data"]["text"])
-            else:
-                necessary_info["thank"].append(0)
 
-            if user_intent == "handover_to_inbox" and len(user_messages_in_conversation) > 1:
-                if message_count_on_day < 2:
+                if user_intent == "handover_to_inbox" and len(user_messages_in_conversation) > 1:
+                    if message_count_on_day < 2:
+                        necessary_info["handover"].append(0)
+                        necessary_info["message"].append(user_message["parse_data"]["text"])
+                        conversation_id_to_remove.append(conversation.sender_id)
+                    else:
+                        necessary_info["handover"].append(1)
+                        necessary_info["message"].append(user_message["parse_data"]["text"])
+                else:
                     necessary_info["handover"].append(0)
-                    necessary_info["message"].append(user_messages_in_conversation[-level]["parse_data"]["text"])
-                    conversation_id_to_remove.append(conversation.sender_id)
-                else:
-                    necessary_info["handover"].append(1)
-                    necessary_info["message"].append(user_messages_in_conversation[-level]["parse_data"]["text"])
-            else:
-                necessary_info["handover"].append(0)
 
-            if user_intent not in ["handover_to_inbox", "thank"] or len(user_messages_in_conversation) <= 1:
-                necessary_info["message"].append("")
-            obj_type = ""
-            if "object_type" in literal_eval(conversation.slots):
-                obj_type = literal_eval(conversation.slots)["object_type"]
-            necessary_info["obj_type"].append(obj_type)
-            message_timestamp_month = datetime.datetime.utcfromtimestamp(
-                int(user_messages_in_conversation[-level]["timestamp"])).strftime('%Y-%m')
-            message_timestamp_date = datetime.datetime.utcfromtimestamp(
-                int(user_messages_in_conversation[-level]["timestamp"])).strftime('%m-%d')
-            message_timestamp = datetime.datetime.utcfromtimestamp(
-                int(user_messages_in_conversation[-level]["timestamp"])).strftime('%Y-%m-%d')
-            necessary_info["message_timestamp_month"].append(message_timestamp_month)
-            necessary_info["message_timestamp_date"].append(message_timestamp_date)
-            necessary_info["message_timestamp"].append(message_timestamp)
-            necessary_info["sender_id"].append(conversation.sender_id)
-            necessary_info["begin"].append(begin_time)
-            necessary_info["end"].append(end_time)
-            necessary_info["level"].append(level)
+                if user_intent not in ["handover_to_inbox", "thank"] or len(user_messages_in_conversation) <= 1:
+                    necessary_info["message"].append("")
+                obj_type = ""
+                if "object_type" in literal_eval(conversation.slots):
+                    obj_type = literal_eval(conversation.slots)["object_type"]
+                necessary_info["obj_type"].append(obj_type)
+                message_timestamp_month = datetime.datetime.utcfromtimestamp(
+                    int(user_message["timestamp"])).strftime('%Y-%m')
+                message_timestamp_date = datetime.datetime.utcfromtimestamp(
+                    int(user_message["timestamp"])).strftime('%m-%d')
+                message_timestamp = datetime.datetime.utcfromtimestamp(
+                    int(user_message["timestamp"])).strftime('%Y-%m-%d')
+                necessary_info["message_timestamp_month"].append(message_timestamp_month)
+                necessary_info["message_timestamp_date"].append(message_timestamp_date)
+                necessary_info["message_timestamp"].append(message_timestamp)
+                necessary_info["sender_id"].append(conversation.sender_id)
+                necessary_info["begin"].append(begin_time)
+                necessary_info["end"].append(end_time)
 
     necessary_info_df = pd.DataFrame.from_dict(necessary_info)
     success_conv = necessary_info_df[necessary_info_df["thank"] == 1]
@@ -179,22 +178,22 @@ def get_success_conv(all_conv_detail, level: int):
 
 def main():
     all_conv_detail = get_all_conv_detail()
-    result_df1, conversation_id_to_remove_1 = get_success_conv(all_conv_detail, level=1)
-    result_df2, conversation_id_to_remove_2 = get_success_conv(all_conv_detail, level=2)
-    result_df3, conversation_id_to_remove_3 = get_success_conv(all_conv_detail, level=3)
-    result_df = pd.concat([result_df3, result_df2, result_df1])
-    result_df = result_df.sort_values(by=["begin", "level"], ascending=False).drop_duplicates(subset=["sender_id"],
-                                                                                              keep="first")
-    conversation_id_to_remove = conversation_id_to_remove_1 + conversation_id_to_remove_2 + conversation_id_to_remove_3
+    result_df1, conversation_id_to_remove_1 = get_success_conv(all_conv_detail)
+    result_df = pd.concat([result_df1])
+    result_df = result_df.sort_values(by=["begin"], ascending=False).drop_duplicates(subset=["sender_id"], keep="first")
+    conversation_id_to_remove = conversation_id_to_remove_1
     conversation_id_to_remove = list(set(conversation_id_to_remove))
     result_sender_id_list = list(result_df["sender_id"])
+
     for item in conversation_id_to_remove:
         if item in result_sender_id_list:
             conversation_id_to_remove.remove(item)
-    all_conv_detail_without_trash = all_conv_detail[~all_conv_detail["sender_id"].isin(conversation_id_to_remove)]
-    all_conv_detail_without_trash = all_conv_detail_without_trash[
-        ~all_conv_detail_without_trash["sender_id"].isin(["default", "me", "abcdef", "123456"])]
-    all_conv_detail_without_trash.to_csv("analyze_data/all_conversations_without_trash.csv", index=False)
+
+    all_sender_id = all_conv_detail[~all_conv_detail["sender_id"].isin(conversation_id_to_remove)]
+    all_sender_id = all_sender_id[~all_sender_id["sender_id"].isin(["default", "me", "abcdef", "123456"])]
+    all_sender_id.to_csv("analyze_data/all_conversations_without_trash.csv", index=False)
+
+    result_df = result_df[~result_df["sender_id"].isin(["default", "me", "abcdef", "123456"])]
     result_df.to_csv("analyze_data/success_conversations.csv", index=False)
 
 
--- plotting.py
+++ plotting.py
@@ -12,82 +12,12 @@ week_list = [datetime.datetime.strptime(x, '%Y%m%d').isocalendar()[1] for x in s
 success_convo.insert(6, 'week', week_list)
 
 
-def line_bar_plot():
-    # Line plot
-    count_success_by_month = success_convo.groupby(["message_timestamp_month"]).count().reset_index()
-    month_list = list(count_success_by_month["message_timestamp_month"])
-    no_sender_id_each_month = list(count_success_by_month["sender_id"])
-
-    fig = go.Figure(data=go.Scatter(x=month_list, y=no_sender_id_each_month, mode='lines+markers'))
-    fig.update_layout(title='Successful conversations by month',
-                      xaxis_title='Month',
-                      yaxis_title='Number of successful conversations')
-    fig.show()
-
-    # Bar plot
-    count_success_by_date = success_convo.groupby(["message_timestamp_date"]).sum().reset_index()
-    date_list = list(count_success_by_date["message_timestamp_date"])
-    no_thank = list(count_success_by_date["thank"])
-    no_handover = list(count_success_by_date["handover"])
-
-    date_count = count_total_convo_on_date()
-    total_conv_on_date = [date_count[x] for x in date_list]
-
-    fig = go.Figure(data=[
-        go.Bar(name='Thanks', x=date_list, y=no_thank, width=0.4,
-               offset=-0.4),
-        go.Bar(name='Order', x=date_list, y=no_handover, width=0.4,
-               offset=-0.4)
-    ])
-
-    fig.update_layout(
-        title='Successfull conversations by date',
-        xaxis_title='Date',
-        yaxis_title='Number of conversations',
-        xaxis=dict(
-            type='category',
-        ),
-        barmode='stack',
-        autosize=False,
-        width=900,
-    )
-    fig.show()
-
-    # Bar plot 2
-    fig = go.Figure(data=[
-        go.Bar(name='Thanks', x=date_list, y=no_thank, width=0.4,
-               offset=-0.4),
-        go.Bar(name='Order', x=date_list, y=no_handover, width=0.4,
-               offset=-0.4)
-    ])
-
-    fig.add_trace(
-        go.Bar(name='Total conversations', x=date_list, y=total_conv_on_date, base=0,
-               width=0.4,
-               offset=0.0,
-               )
-    )
-    fig.update_layout(
-        title='Successful conversations by date on total',
-        xaxis_title='Date',
-        yaxis_title='Number of conversations',
-        xaxis=dict(
-            type='category',
-        ),
-        barmode='stack',
-        autosize=False,
-        width=950,
-    )
-    fig.update_xaxes(tickangle=45)
-    fig.show()
-
-
 def bar_plot_month():
     count_success_by_month = success_convo.groupby(["message_timestamp_month"]).count().reset_index()
     month_list = list(count_success_by_month["message_timestamp_month"])
     no_sender_id_each_month = list(count_success_by_month["sender_id"])
     month_count = count_total_convo_on_month()
-    total_conv_on_date = [month_count[x] for x in [x.split("-")[1] for x in month_list]]
+    total_conv_on_month = [month_count[x] for x in [x.split("-")[1] for x in month_list]]
 
     fig = go.Figure(data=go.Bar(x=month_list, y=no_sender_id_each_month, text=no_sender_id_each_month))
     fig.update_traces(textposition='outside')
@@ -100,7 +30,7 @@ def bar_plot_month():
 
     fig = go.Figure(data=[
         go.Bar(name="Successful conversations",x=month_list, y=no_sender_id_each_month, text=no_sender_id_each_month),
-        go.Bar(name="Total", x=month_list, y=total_conv_on_date, text=total_conv_on_date),
+        go.Bar(name="Total", x=month_list, y=total_conv_on_month, text=total_conv_on_month),
     ])
     fig.update_traces(textposition='outside')
     fig.update_layout(uniformtext_minsize=8, uniformtext_mode='hide')
@@ -116,9 +46,9 @@ def bar_plot_week():
     # Bar plot week 1
     count_success_by_week = success_convo.groupby(["week"]).sum().reset_index()
     week_list = list(count_success_by_week["week"])
+    week_list = week_list[:-1]
     no_thank = list(count_success_by_week["thank"])
     no_handover = list(count_success_by_week["handover"])
-    week_count = count_total_convo_on_week()
 
     fig = go.Figure(data=[
         go.Bar(name='Thanks', x=week_list, y=no_thank),
@@ -143,6 +73,7 @@ def bar_plot_week():
     # Bar plot week 2
     count_success_by_week = success_convo.groupby(["week"]).sum().reset_index()
     week_list = list(count_success_by_week["week"])
+    week_list = week_list[:-1]
     no_thank = list(count_success_by_week["thank"])
     no_handover = list(count_success_by_week["handover"])
     week_count = count_total_convo_on_week()
@@ -181,9 +112,11 @@ def bar_plot_week():
 
 
 def sun_burst():
-    success_rate = (len(success_convo) / len(all_convo)) * 100
+    all_conversation = count_total_convo_on_date()
+    all_conversation_count = sum(list(all_conversation.values()))
+    success_rate = (len(success_convo) / all_conversation_count) * 100
     max_turn, min_turn, mean_turn, top_10_turn_happen_most, top_10_highest_no_turn, comback_customer = count_turn()
-    print("All Conversations: " + str(len(all_convo)))
+    print("All Conversations: " + str(all_conversation_count))
     print("Successful Conversations: " + str(len(success_convo)))
     print("Success rate: " + '{0:.2f}'.format(success_rate) + "%")
     print("Average number of turn: " + str(mean_turn))
@@ -196,7 +129,7 @@ def sun_burst():
     fig = go.Figure(go.Sunburst(
         labels=["Success", "All conversation", "Thank", "Order"],
         parents=["", "", "Success", "Success"],
-        values=[no_success, no_all_convo, no_thank, no_handover],
+        values=[no_success, all_conversation_count, no_thank, no_handover],
         insidetextorientation='radial'
 
     ))
@@ -216,12 +149,9 @@ def count_total_convo_on_date():
     all_date = []
     for item in events_list:
         event = literal_eval(item)
-        set([int(x["timestamp"]) for x in event])
-        all_date += list(set(
-            [datetime.datetime.utcfromtimestamp(int(x["timestamp"])).strftime('%m-%d') for x in literal_eval(item)]))
-
+        user_event = [x for x in event if x["event"] == "user"]
+        all_date += list(set([datetime.datetime.utcfromtimestamp(int(x["timestamp"])).strftime('%m-%d') for x in user_event]))
     date_counter = Counter(all_date)
-
     return date_counter
 
 
@@ -230,40 +160,35 @@ def count_total_convo_on_week():
     all_date = []
     for item in events_list:
         event = literal_eval(item)
-        set([int(x["timestamp"]) for x in event])
-        all_date += list(set(
-            [datetime.datetime.utcfromtimestamp(int(x["timestamp"])).strftime('%Y-%m-%d') for x in literal_eval(item)]))
+        user_event = [x for x in event if x["event"] == "user"]
+        all_date += list(set([datetime.datetime.utcfromtimestamp(int(x["timestamp"])).strftime('%Y-%m-%d') for x in user_event]))
     date_counter = Counter(all_date)
     date_counter_list = [(x, y) for x, y in date_counter.items()]
     date_counter_list = sorted(date_counter_list, key=lambda x: x[0])
-    week = None
-    week_value = 0
-    total_convo_on_week = []
-    for index, item in enumerate(date_counter_list):
-        if week is None:
-            week = datetime.datetime.strptime(item[0], '%Y-%m-%d').isocalendar()[1]
-        if week == datetime.datetime.strptime(item[0], '%Y-%m-%d').isocalendar()[1]:
-            week_value += item[1]
-        else:
-            total_convo_on_week.append((week, week_value))
-            week_value = 0
-            week = datetime.datetime.strptime(item[0], '%Y-%m-%d').isocalendar()[1]
-        if index == len(date_counter_list) - 1:
-            total_convo_on_week.append((week, week_value))
 
-    week_counter = {item[0]: item[1] for item in total_convo_on_week}
-
-    return week_counter
+    week_counter_list = [(datetime.datetime.strptime(item[0], '%Y-%m-%d').isocalendar()[1],item[1]) for item in date_counter_list]
+    week_counter = {x[0] for x in week_counter_list}
+    week_counter_sum = [(i, sum(x[1] for x in week_counter_list if x[0] == i)) for i in week_counter]
+    week_counter_result = {item[0]: item[1] for item in week_counter_sum}
+    return week_counter_result
 
 
 def count_total_convo_on_month():
     events_list = list(all_convo["events"])
-    all_month = []
+    all_date = []
     for item in events_list:
         event = literal_eval(item)
-        all_month += list(set([datetime.datetime.utcfromtimestamp(int(x["timestamp"])).strftime('%m') for x in event]))
-    month_counter = Counter(all_month)
-    return month_counter
+        user_event = [x for x in event if x["event"] == "user"]
+        all_date += list(set([datetime.datetime.utcfromtimestamp(int(x["timestamp"])).strftime('%Y-%m-%d') for x in user_event]))
+    date_counter = Counter(all_date)
+    date_counter_list = [(x, y) for x, y in date_counter.items()]
+    date_counter_list = sorted(date_counter_list, key=lambda x: x[0])
+
+    month_counter_list = [(item[0].split("-")[1], item[1]) for item in date_counter_list]
+    month_counter = {x[0] for x in month_counter_list}
+    month_counter_sum = [(i, sum(x[1] for x in month_counter_list if x[0] == i)) for i in month_counter]
+    month_counter_result = {item[0]: item[1] for item in month_counter_sum}
+    return month_counter_result
 
 
 def count_turn():
@@ -287,8 +212,6 @@ def count_turn():
     sorted_counter_turn_by_key = OrderedDict(sorted_counter_turn_by_value)
     top_10_turn_happen_most = list(sorted_counter_turn_by_value.items())[:10]
     top_10_highest_no_turn = list(sorted_counter_turn_by_key.items())[:10]
-
     return max_turn, min_turn, mean_turn, top_10_turn_happen_most, top_10_highest_no_turn, comeback_customer
 
-
-bar_plot_month()
+count_total_convo_on_week()
